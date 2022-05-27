a533b43e48303ef5a9b4580664b3aca648b5feaa
Artem Chubenko <artem.chubenko@gmail.com>
2022-05-27 12:29:48 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,46 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const refList = document.querySelector('#ingredients');
+
+
+const ingridienteItem = 
+  ingredients.map(option => {  
+    const item = document.createElement('li');
+    item.classList.add("item");
+    item.textContent = option;
+
+    return item
+
+})
+console.log(ingridienteItem);
+refList.append(...ingridienteItem);
+
+
+
+
+
+// console.log(refList);
+
+
+
+// item.textContent = ingredients[0];
+
+
+// const link = document.createElement('a');
+// link.classList.add = "link";
+// link.textContent = "This id link text";
+// link.href = './index.html'
+// item.appendChild(link);
+
+// console.log(item);
+// console.log(link);
+
+// refList.appendChild(item);
+// refList.append(item);
+// refList.insertBefore(item, null);
+
+
+
