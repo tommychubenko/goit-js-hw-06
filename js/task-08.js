dc0d3f9b0f98bbdfa9d4a3ae65bfd2d0ca98ca3d
Artem Chubenko <artem.chubenko@gmail.com>
2022-05-29 00:09:48 +0300

    DONE

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,28 @@
+const form = document.querySelector('.login-form');
+// console.log(form);
+// const submitBtn = document.querySelector('[type=submit]');
+// console.log(submitBtn);
+const userData = [];
 
+function submitting(event){
+    event.preventDefault();
+
+    const { email, password } = form;
+  
+    if (email.value && password.value !== "" )
+    
+    {
+        console.log('Hurray! You succesfully enterd a valid data!')
+        userData.push({ 'email': email.value, 'password': password.value }),
+    console.log('Now, in user list we have next:', userData)}
+
+    else(alert("Ooops.... Please fill all the fields!"))
+
+    
+    // console.log('Pushed');
+    // console.log(form.email.value)
+
+};
+
+
+form.addEventListener("submit", submitting);
