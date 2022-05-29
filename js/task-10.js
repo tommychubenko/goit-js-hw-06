ebb6f888285442f7737f69337f5d781421674491
Artem Chubenko <artem.chubenko@gmail.com>
2022-05-29 23:54:34 +0300

    DONE

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const input = document.querySelector('[type=number]');
+const createBtn = document.querySelector('[data-create]')
+const destroyBtn = document.querySelector('[data-destroy]')
+const mainDiv = document.querySelector('#boxes');
+
+const warehouseOdDivs = [];
+
+
+
+function createBoxes(event) {  
+  for (let i = 0; i < input.value; i += 1){
+    let randomColor = getRandomHexColor();
+    const randomDiv = document.createElement("div");    
+    randomDiv.style.backgroundColor = randomColor;
+    randomDiv.style.height = `${i * 10 + 30}px`;
+    randomDiv.style.width = `${i * 10 + 30}px`;
+    warehouseOdDivs.push(randomDiv);    
+  };  
+
+  mainDiv.append(...warehouseOdDivs);
+  warehouseOdDivs.length = 0;
+};
+ 
+function destroyBoxes(event) {
+   mainDiv.innerHTML = "";  
+  }
+
+
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes)
