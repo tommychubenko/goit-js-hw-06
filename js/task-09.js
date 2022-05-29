4cd8d99868924ffb84187070f00bf3a0deaea467
Artem Chubenko <artem.chubenko@gmail.com>
2022-05-29 12:20:58 +0300

    DONE

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
+const colorInfo = document.querySelector('.widget span');
+
+
+const changeColorBtn = document.querySelector('.widget button');
+
+const body = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+function changeColor() {
+  const randomColor = getRandomHexColor()
+  body.style.backgroundColor = randomColor;
+  colorInfo.textContent = randomColor;
+  console.log(`Генерим новий колір... ${randomColor}`)
+ }
+
+changeColorBtn.addEventListener('click', changeColor)
+
+
+
+
