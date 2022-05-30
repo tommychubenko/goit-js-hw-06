90e719b1491ca9835a99355874465c21d7f8f2aa
Artem Chubenko <artem.chubenko@gmail.com>
2022-05-30 18:15:09 +0300

    Done

--- js/task-01.js
+++ js/task-01.js
@@ -1,23 +1,11 @@
-// const list = document.querySelector('#categories');
-// const numberOfCategories = list.children.length;
-
-const list = document.querySelectorAll('li');
-const allList = document.querySelectorAll('ul')
+const allList = document.querySelectorAll('ul:not(#categories')
 const categories = document.querySelectorAll('h2');
 
 const categoriesList = [];
 categories.forEach(element => categoriesList.push(element.textContent));
 console.log(`Number of categories: ${categoriesList.length}`);
 
-
-const listAnimals = [];
-allList.forEach(element => listAnimals.push(element.children));
-listAnimals.shift();
-
-
-console.log(`Category: ${categoriesList[0]}`);
-console.log(`Elements: ${listAnimals[0].length}`);
-console.log(`Category: ${categoriesList[1]}`);
-console.log(`Elements: ${listAnimals[1].length}`);
-console.log(`Category: ${categoriesList[2]}`);
-console.log(`Elements: ${listAnimals[2].length}`);
+allList.forEach(element => {
+    console.log(`Category: ${element.previousElementSibling.textContent}`),
+        console.log(`Elements: ${element.children.length}`)
+}) 
